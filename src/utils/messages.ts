b69dc656c15fb3d481c33f93751bc4651b3ad72b
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-02-04 17:45:29 +0300

    correct messages (create two object and remove a lot of variables)

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,13 +1,18 @@
-export const warningMessage = 'With this step it is impossible to get the max value'
-export const errorMessageGreaterMax = 'This value must be greater than the min value'
-export const errorMessageLessMin = 'This value must be less than the max value'
-export const errorMessageLength = "It's number very big"
-export const errorMessageGreaterStep = 'The step must be less than the maximum number'
-export const errorMessageDenyStep = 'The step must be greater than zero'
-export const errorMessageGreaterMaxForStep = 'The max value must be greater than the step'
+export const messages = {
+    warning: 'With this step it is impossible to get the max value',
+    greaterMax: 'This value must be greater than the min value',
+    greaterStep: 'The step must be less than the maximum number',
+    lessMin: 'This value must be less than the max value',
+    bigLength: "It's number very big",
+    denyStep: 'The step must be greater than zero',
+    greaterMaxForStep: 'The max value must be greater than the step',
+}
 
-export const notificationRandom = 'Random settings set'
-export const notificationDefault = 'Default settings set'
-export const notificationSave = 'Settings have been saved'
-export const notificationForget = "Don't forget to save your changes"
-export const notificationCan = 'You can manage settings'
+export const notifications = {
+    random: 'Random settings set',
+    default: 'Default settings set',
+    save: 'Settings have been saved',
+    forget: 'Don\'t forget to save your changes',
+    can: 'You can manage settings',
+
+}
